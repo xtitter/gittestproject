147a7cb144cdaabaf494446839c0bc9407916287
Nathaniel Titterton <nate@berkeley.edu>
2015-11-25 19:45:09 -0800

    lots of fun

--- src/one/MyClass.java
+++ src/one/MyClass.java
@@ -6,5 +6,6 @@ public class MyClass {
     
     public static void main(String[] args) {
         System.out.println("Here I am in my own class.");
+        System.out.println("A second line");
     }
 }
